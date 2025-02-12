46e74e88468050cb81428a0b8c2add6494b57960
Andjela <andjelapetkovic412@gmail.com>
2025-02-12 16:48:48 +0100

    Modified index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Kitchen</title>
+</head>
+
+<body>
+      <ul>
+            <li><a href="">New allergen</a></li>
+            <li><a href="">New recipe</a></li>
+            <li><a href="">New taster</a></li>
+            <li><a href="">New login</a></li>
+      </ul>
+</body>
+
+</html>
